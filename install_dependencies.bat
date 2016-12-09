9e5a1404a3ff367cf40c6fa9a5e94b96fa0735ce
Kasuporo <aphelion030@gmail.com>
2016-12-09 19:54:47 +1100

    Delete install_dependencies.bat

--- install_dependencies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM This batch files installs a bunch of python libraries needed for a search engine
-pip install beautifulsoup4
-pip install lxml
-pip install urllib3
-pip install flask
-
-echo Dependencies installed
-PAUSE
