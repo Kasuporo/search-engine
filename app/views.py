a873c5adda31f3b82f7256b473f3fe4c58cfde8c
Kasuporo <aphelion030@gmail.com>
2016-12-11 14:39:05 +1100

    Minor changes

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from app import app
 def index():
 	if request.method == 'POST':
 		query = request.form['search']
-		return render_template('results.html', query = query)
+		return render_template('results.html',
+								query = query)
 	else:
 		return render_template('queryindex.html')
