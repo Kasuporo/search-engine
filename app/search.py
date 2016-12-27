b8d5a4546b0f821488b8cfbfb927357e4252c24a
Kasuporo <aphelion030@gmail.com>
2016-12-27 17:51:52 +1100

    Commit-ed

--- app/search.py
+++ app/search.py
@@ -4,19 +4,30 @@ import lxml.html
 import re
 from bs4 import BeautifulSoup
 
-class Web(Object):
+class web(Object):
 
-	def __init__(self, query):
+	def __init__(self, query, seed, depth):
 		self.query = query
+		self.seed = seed
+		self.depth = depth
 
 	# Gets all links from a page and puts it into a list
-	def find_all_links(link):
+	def find_all_links(self, link):
 		connection = urllib2.urlopen(link)
 		dom = lxml.html.fromstring(connection.read())
 		url = [link for link in dom.xpath('//a/@href') if link.startswith('http')]
 		# Ignores relative links becuause they are bad
 		return url
 	#print(find_all_links('https://www.raspberrypi.org/forums/viewtopic.php?f=32&t=55260'))
+
+	# Gets all text in a <p> tag from a html page
+	def get_text(self, link):
+		html = urllib2.urlopen(link)
+		soup = BeautifulSoup(html, 'lxml')
+		pTexts = [p.get_text() for p in soup.find_all('p')]
+		pTexts = " ".join(pTexts)
+		return pTexts
+	#print(get_text('http://news.bbc.co.uk/2/hi/health/2284783.stm'))
 
 	# Web crawler finds ALL the links from the web up to a set depth
 	def web_crawl(self, seed, maxDepth):
@@ -35,16 +46,7 @@ class Web(Object):
 		return crawled
 	#print(web_crawl('http://www.qantas.com/qcatering/quality-safety-environment/index.html', 2))
 
-	# Gets all text in a <p> tag from a html page
-	def get_text(link):
-		html = urllib2.urlopen(link)
-		soup = BeautifulSoup(html, 'lxml')
-		pTexts = [p.get_text() for p in soup.find_all('p')]
-		pTexts = " ".join(pTexts)
-		return pTexts
-	#print(get_text('http://news.bbc.co.uk/2/hi/health/2284783.stm'))
-
-class Text(Object):
+class text(Object):
 
 	def __init__(self):
 		pass
--- app/views.py
+++ app/views.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, request, redirect, g
 
-from search import Web, Text
 from app import app
+from search import web, text
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/web', methods=['GET', 'POST'])
+def web_search():
 	if request.method == 'POST':
 		query = request.form['search']
 		# Put query here
+		# webSearch = web(query, seed, depth)
+		# results = webSearch.web_query()
 		return render_template('results.html',
-								query = query,
+								results = results,
 								title = "Results")
 	else:
 		return render_template('web_search.html')
@@ -19,8 +25,11 @@ def index():
 def text_search():
 	if request.method == 'POST':
 		query = request.form['search']
+		# Here too
+		# textSearch = text(query, docs)
+		# results = textSearch.text_query()
 		return render_template('results.html',
-								query = query,
+								results = results,
 								title = 'Results')
 	else:
 		return render_template('text_search.html')
