f3ac7474ba66de6aa05101d85da6516385e0ee38
justin <aphelion030@gmail.com>
2017-02-21 18:42:28 +1100

    ADDED NEW HOME PAGE

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from app import app
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return render_template('index.html')
 
 @app.route('/web', methods=['GET', 'POST'])
 def web_search():
