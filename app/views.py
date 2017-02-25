89c89739d2e75c8b0e9761ff49a5a27beceda4ff
justin <aphelion030@gmail.com>
2017-02-25 17:57:37 +1100

    MORE CSS

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, g
+from flask import Flask, render_template, request, redirect
 
 import feedparser
 import re
@@ -18,31 +18,31 @@ def home():
 @app.route('/results', methods=['GET', 'POST'])
 def web_search():
 	if request.method == 'POST':
-		""" if request.form.get('external'):
+		if request.form.get('external'):
 			external = True
 		else:
 			external = False
 
-		seed, depth = request.form['seed'], request.form['depth']
+		# seed, depth = request.form['seed'], request.form['depth']
 		# Gets stuff for web search
-		docs = request.form['docs']
+		# docs = request.form['docs']
 		# Gets stuff for text search
-		query = request.form['query'] """
+		query = request.form['query']
 
 		# textSearch = text(query, docs)
 		# webSearch = web(query, seed, depth, external)
 
 		#TO-DO: Merge text and web search into one big search
 
-		backgrounds = ['http://i.imgur.com/HSEvn6M.jpg', 'http://i.imgur.com/wYekTr5.jpg', 'http://i.imgur.com/AdlyZgO.jpg'
+		backgrounds = ['http://i.imgur.com/HSEvn6M.jpg', 'http://i.imgur.com/wYekTr5.jpg', 'http://i.imgur.com/AdlyZgO.jpg',
 					   'http://i.imgur.com/I0zYjsT.jpg', 'http://i.imgur.com/I0zYjsT.jpg', 'http://i.imgur.com/mj2QAev.jpg']
 
-		number = random.randint(0,4)
+		number = random.randint(0,5)
 		background = backgrounds[number]
 
 		return render_template('results.html',
-								background = background
-								#query = query,
+								background = background,
+								query = query,
 								#results = results,
 								#title = "Results"
 								)
