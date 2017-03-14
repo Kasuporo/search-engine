51ce66438b6b601c271fc8eb803319d01e16a679
justin <aphelion030@gmail.com>
2017-03-14 17:13:02 +1100

    did i add ?

--- app/search.py
+++ app/search.py
@@ -7,61 +7,61 @@ from bs4 import BeautifulSoup
 
 class web:
 
-	def __init__(self, query, seed, depth, external):
-		self.query = query
-		self.seed = seed
-		self.depth = depth
-		self.external = external
+    def __init__(self, query, seed, depth, external):
+        self.query = query
+        self.seed = seed
+        self.depth = depth
+        self.external = external
 
-	def find_all_links(self):
-		http = httplib2.Http()
-		status, response = http.request(link)
-		page = html.fromstring(response)
+    def find_all_links(self):
+        http = httplib2.Http()
+        status, response = http.request(link)
+        page = html.fromstring(response)
 
-		# Finds every link on a webpage
-		if not self.external:
-			url = [link for link in page.xpath('//a/@href') if link.startswith('http')]
-			# Ignores relative links becuause they are bad
-		else:
-			parsedUri = urlparse(link)
-			domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsedUri)
-			url = [link for link in page.xpath('//a/@href') if link.startswith(domain)]
-		return url
+	# Finds every link on a webpage
+        if not self.external:
+            url = [link for link in page.xpath('//a/@href') if link.startswith('http')]
+            # Ignores relative links becuause they are bad
+        else:
+            parsedUri = urlparse(link)
+            domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsedUri)
+            url = [link for link in page.xpath('//a/@href') if link.startswith(domain)]
+        return url
 
-	def get_info(self, link):
-		http = httplib2.Http()
-		status, response = http.request(link)
-		soup = BeautifulSoup(response, 'lxml')
+    def get_info(self, link):
+        http = httplib2.Http()
+        status, response = http.request(link)
+        soup = BeautifulSoup(response, 'lxml')
 
-		# Gets title and all text in a <p> tag
-		title = soup.title.name
-		pTexts = [p.get_text() for p in soup.find_all('p')]
-		pTexts = "".join(pTexts)
-		return title, pTexts
+	# Gets title and all text in a <p> tag
+        title = soup.title.name
+        pTexts = [p.get_text() for p in soup.find_all('p')]
+        pTexts = "".join(pTexts)
+        return title, pTexts
 
-	def web_crawl(self):
-		toCrawl = [self.seed]
-		crawled = []
-		nextDepth = []
-		atDepth = 0
-		while toCrawl and atDepth <= self.depth:
-			page = toCrawl.pop()
-			if page not in crawled:
-				nextDepth += find_all_links(page)
-				crawled.append(page)
-			if not toCrawl:
-				toCrawl, nextDepth = nextDepth, []
-				atDepth += 1
-		return crawled
+    def web_crawl(self):
+        toCrawl = [self.seed]
+        crawled = []
+    	nextDepth = []
+        atDepth = 0
+    	while toCrawl and atDepth <= self.depth:
+            page = toCrawl.pop()
+            if page not in crawled:
+                nextDepth += find_all_links(page)
+                crawled.append(page)
+            if not toCrawl:
+                toCrawl, nextDepth = nextDepth, []
+    		atDepth += 1
+    	return crawled
 
-		def index(self):
-			# Return as: {URL : [Title, Text]}
-			pages = web_crawl()
-			title, text = [get_info(url) for url in pages]
-			# TO-DO: Index on text
+    def index(self):
+        # Return as: {URL : [Title, Text]}
+        pages = web_crawl()
+        title, text = [get_info(url) for url in pages]
+        # TO-DO: Index on text
 
 class text:
 
-	def __init__(self, query, docs):
-		self.query = query
-		self.docs = docs
+    def __init__(self, query, docs):
+        self.query = query
+        self.docs = docs
--- app/views.py
+++ app/views.py
@@ -8,44 +8,42 @@ from app import app
 
 @app.route('/')
 def home():
-	feedURL = "https://www.nasa.gov/rss/dyn/image_of_the_day.rss"
-	feed = feedparser.parse(feedURL)
-	background = feed.entries[0]['links'][1]['href']
-	# Gets url of NASA image of the day
-	return render_template('index.html',
-				background = background)
+    feedURL = "https://www.nasa.gov/rss/dyn/image_of_the_day.rss"
+    feed = feedparser.parse(feedURL)
+    background = feed.entries[0]['links'][1]['href']
+    # Gets url of NASA image of the day
+    return render_template('index.html',
+                           background = background)
 
 @app.route('/results', methods=['GET', 'POST'])
 def web_search():
-	if request.method is 'POST':
-		if request.form.get('external'):
-			external = True
-		else:
-			external = False
+    if request.method is 'POST':
+        if request.form.get('external'):
+            external = True
+        else:
+             external = False
 
-		# seed, depth = request.form['seed'], request.form['depth']
-		# Gets stuff for web search
-		# docs = request.form['docs']
-		# Gets stuff for text search
-		query = request.form['query']
+	# seed, depth = request.form['seed'], request.form['depth']
+	# Gets stuff for web search
+	# docs = request.form['docs']
+	# Gets stuff for text search
+        query = request.form['query']
 
-		# textSearch = text.index(query, docs)
-		# webSearch = web.index(query, seed, depth, external)
+	# textSearch = text.index(query, docs)
+	# webSearch = web.index(query, seed, depth, external)
 
-		#TO-DO: Merge text and web search into one big search
+	#TO-DO: Merge text and web search into one big search
 
-		backgrounds = ['http://i.imgur.com/HSEvn6M.jpg', 'http://i.imgur.com/wYekTr5.jpg',
-			       'http://i.imgur.com/AdlyZgO.jpg', 'http://i.imgur.com/I0zYjsT.jpg',
-			       'http://i.imgur.com/I0zYjsT.jpg', 'http://i.imgur.com/mj2QAev.jpg']
+        backgrounds = ['http://i.imgur.com/HSEvn6M.jpg', 'http://i.imgur.com/wYekTr5.jpg',
+	           'http://i.imgur.com/AdlyZgO.jpg', 'http://i.imgur.com/I0zYjsT.jpg',
+	           'http://i.imgur.com/I0zYjsT.jpg', 'http://i.imgur.com/mj2QAev.jpg']
 
-		number = random.randint(0,5)
-		background = backgrounds[number]
-		# Sets random background from list
+        number = random.randint(0,5)
+        background = backgrounds[number]
+	# Sets random background from list
 
-		return render_template('results.html',
-					background = background,
-					query = query)
-	else:
-		redirect("/", code=302)
-
-
+        return render_template('results.html',
+	    	               background = background,
+                               query = query)
+    else:
+        redirect("/", code=302)
