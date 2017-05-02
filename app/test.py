d921091a4038e7e11bd3813ba067c0a343f5b069
justin <aphelion030@gmail.com>
2017-05-02 16:23:38 +1000

    i dont need that

--- app/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-""" This is a test file for testing individual functions
-    before I put them in """
-import feedparser
-
-feedURL = "https://www.nasa.gov/rss/dyn/image_of_the_day.rss"
-feed = feedparser.parse(feedURL)
-print(feed.entries[0]['links'][1]['href'])
